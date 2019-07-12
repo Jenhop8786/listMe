34db1fe648962aa4e0ff900ca1e7a0b5d56f29a4
J Jjhopkins J Hopkins <jenelyn.hopkins@gmail.com>
2019-07-11 22:14:25 -0700

    Added ability to store real contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,7 +14,22 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def create
+    @contact = Contact.new(contact_params)
+     if @contact.save
+       flash[:success] = "Contact Saved"
+       redirect_to contacts_path
+     else
+       render 'new'
+     end
+  end
 
+
+private
+
+def contact_params
+   params.require(:contact).permit(:name, :email, :company, :phone, :group_id)
+end
 
 
 end#class
