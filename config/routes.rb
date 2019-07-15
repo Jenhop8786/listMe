1b468a177514f6b2e01df86ddce11bca0e525e55
J Jjhopkins J Hopkins <jenelyn.hopkins@gmail.com>
2019-07-15 07:27:43 -0700

    Fixed bundler error and added delete function

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'contacts/create', as: 'create_contact'
   get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
 
+  delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
   patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
   # resources :contacts
 
