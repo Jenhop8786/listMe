aa90662fa1009c0508b43673a732a29db7811daf
J Jjhopkins J Hopkins <jenelyn.hopkins@gmail.com>
2019-07-17 10:30:39 -0700

    Implemented multi users support in the application

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,11 +6,11 @@ class ContactsController < ApplicationController
 
   def index
     session[:selected_group_id] = params[:group_id]
-    @contacts = Contact.by_group(params[:group_id]).search(params[:term]).order(created_at: :desc).page(params[:page])
+    @contacts = current_user.contacts.by_group(params[:group_id]).search(params[:term]).order(created_at: :desc).page(params[:page])
   end
 
   def autocomplete
-    @contacts = Contact.search(params[:term]).order(created_at: :desc).page(params[:page])
+    @contacts = current_user.contacts.search(params[:term]).order(created_at: :desc).page(params[:page])
     render json: @contacts.map { |contact| { id: contact.id, value: contact.name } }
   end
 
@@ -19,7 +19,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params)
+    @contact = current_user.contacts.build(contact_params)
     if @contact.save
       flash[:success] = 'Contact Saved'
       redirect_to contacts_path(previous_query_string)
