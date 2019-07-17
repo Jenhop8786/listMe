2a7b7499b47048251b75983ee14c35592f8faf5b
J Jjhopkins J Hopkins <jenelyn.hopkins@gmail.com>
2019-07-17 08:10:26 -0700

    Created helper method to indicate menu item is active based on the uri segment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def active_item(uri)
+    uri_segments = request.fullpath.split(/\/|\?/)
+    uri_segments[1] === uri ? 'active' : ''
+  end
+
 end
