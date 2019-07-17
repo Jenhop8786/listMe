404b36157a3448ad94fada03fca41d2a577f6f75
J Jjhopkins J Hopkins <jenelyn.hopkins@gmail.com>
2019-07-17 06:37:01 -0700

    Customized navbar in application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap/dropdown
 //= require jasny-bootstrap.min
 //= require jquery_ujs
 //= require turbolinks
